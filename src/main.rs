c5ea73e13a12dd2172efe99c08028458fa3fe215
paragkoche <paragkoche0@gmail.com>
2024-02-13 17:44:46 +0530

    add datatype

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod basic {
+    pub mod datatype;
     pub mod hello_wold;
 }
 
 // use basic::hello_wold
 fn main() {
-    basic::hello_wold::hello_world();
+    // basic::hello_wold::hello_world();
+    basic::datatype::datatype()
 }
